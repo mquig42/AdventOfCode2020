bf57d9369c916faca7166158bc72eb353ff2aed1
mquig42 <mquig42@users.noreply.github.com>
2020-12-02 11:01:18 -0500

    Refactored AoC_2
    Now less redundant, fewer lines. Also, you can add a bool to an int. True=1, False=0. That could be useful later.

--- AoC_2.py
+++ AoC_2.py
@@ -7,16 +7,10 @@
 ################################################################################
 import time
 
-def isValid(policy, pw):
-    lo = (int)(policy.split(' ')[0].split('-')[0])
-    hi = (int)(policy.split(' ')[0].split('-')[1])
-    ch = policy.split(' ')[1]
+def isValid(lo, hi, ch, pw):
     return (pw.count(ch) >= lo) and (pw.count(ch) <= hi)
 
-def isValid2(policy, pw):
-    lo = (int)(policy.split(' ')[0].split('-')[0])
-    hi = (int)(policy.split(' ')[0].split('-')[1])
-    ch = policy.split(' ')[1]
+def isValid2(lo, hi, ch, pw):
     clo = pw[lo-1]
     chi = pw[hi-1]
     return (clo == ch and chi != ch) or (clo != ch and chi == ch)
@@ -29,11 +23,13 @@ count = 0
 count2 = 0
 inp = open('Input2.txt')
 for line in inp:
-    if isValid(line.split(': ')[0],line.split(': ')[1]):
-        count = count + 1
-    if isValid2(line.split(': ')[0],line.split(': ')[1]):
-        count2 = count2 + 1
+    policy = line.split(': ')[0]
+    pw = line.split(': ')[1]
+    rg = policy.split(' ')[0].split('-')
+    count += isValid((int)(rg[0]), (int)(rg[1]), policy.split(' ')[1], pw)
+    count2 += isValid2((int)(rg[0]), (int)(rg[1]), policy.split(' ')[1], pw)
+
+inp.close()
 
 print(count, count2)
-
 print('Elapsed Time:', '{0:0.3f}s'.format(time.time() - start_time))
