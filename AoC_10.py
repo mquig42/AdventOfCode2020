17a53ffad59525b00b56edf7e71df38a52a36851
mquig42 <mquig42@users.noreply.github.com>
2020-12-10 17:28:51 -0500

    Easier way to do 10-2
    Turns out you don't actually need a directed acyclic graph.

--- AoC_10.py
+++ AoC_10.py
@@ -8,12 +8,18 @@
 #
 # Part 1 is easy, just sort the list and iterate through once, comparing each
 # value to the previous.
+#
 # Part 2 was more difficult. After some thought, and reviewing some graph theory
 # I decided to model the possible connections between adapters as a directed
 # acyclic graph. In theory, a depth first search could count each path, but
 # would probably be extremely slow. Instead, I went through the adapter list
 # in reverse order, caching the number of paths. That way I could just sum up
 # all the children.paths instead of actually exploring each path to the end.
+#
+# Turns out I was actually making part 2 a lot more complicated than it needed
+# to be. Implemented it in another way. 6 lines vs 16, using a simple array
+# vs a more complex graph structure. It's actually possible to solve both parts
+# of today's challenge using only a spreadsheet.
 ################################################################################
 import time
 
@@ -52,8 +58,7 @@ def countPaths(adapters):
             nodes[a].children.append(nodes[a+3])
 
     #Iterate backwards through list
-    adapters.reverse()
-    for a in adapters:
+    for a in reversed(adapters):
         if len(nodes[a].children) == 0:
             nodes[a].paths = 1
         else:
@@ -61,6 +66,17 @@ def countPaths(adapters):
                 nodes[a].paths += c.paths
 
     return nodes[0].paths
+
+#Solve part 2 in a much simpler way
+def countPaths2(adapters):
+    paths = [0 for i in range(len(adapters))]
+    paths[len(adapters)-1] = 1
+    for i in range(len(adapters)-2, -1, -1):
+        for j in range(1, 4):
+            if (i + j) < len(adapters) and (adapters[i+j]-adapters[i]) <= 3:
+                paths[i] += paths[i+j]
+    return paths[0]
+            
 
 #Entry point
 start_time = time.time()
@@ -74,6 +90,7 @@ adapters.append(max(adapters)+3)
 adapters.sort()
 
 print('Part 1:', count13(adapters))
-print('Part 2:', countPaths(adapters))
+print('Part 2 (hard way):', countPaths(adapters))
+print('Part 2 (easy way):', countPaths2(adapters))
 
 print('Elapsed Time:', '{0:0.3f}s'.format(time.time() - start_time))
